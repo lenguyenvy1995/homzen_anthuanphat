ce1ae74232fb0cf43fa874ef41bb8e5c8b9002d3
lenguyenvy1995 <42138419+lenguyenvy1995@users.noreply.github.com>
2025-12-19 09:50:29 +0700

    btn + introducation finish

--- platform/themes/homzen/partials/shortcodes/about-us/index.blade.php
+++ platform/themes/homzen/partials/shortcodes/about-us/index.blade.php
@@ -11,29 +11,38 @@
     }
 @endphp
 
-<section class="flat-banner-about shortcode-customer py-5" style="{{ $backgroundStyle }}">
+<section class="shortcode-customer shortcode-customer-about flat-banner-about  py-5" style="{{ $backgroundStyle }}">
     <div class="container">
         <div class="row">
-            <div class="col-md-6 my-auto">
-                @if ($shortcode->title)
-                    <h2>{!! BaseHelper::clean($shortcode->title) !!}</h2>
-                @endif
-                @if ($shortcode->description)
-                    <p class="body-2 text-variant-1">
-                        {!! BaseHelper::clean(nl2br($shortcode->description)) !!}
-                    </p>
-                @endif
-                <div class="hover-btn-view">
-                    @if ($shortcode->button_label)
-                        <a href="{{ $shortcode->button_url }}" class="btn-view style-1">
-                            <span class="text">{{ $shortcode->button_label }}</span>
-                            <span class="icon icon-arrow-right2"></span>
-                        </a>
+            <div class="col-md-6 my-auto ">
+                <div class="about-cotent">
+                    @if ($shortcode->title)
+                        <h2 class="about-cotent-title">{!! BaseHelper::clean($shortcode->title) !!}</h2>
                     @endif
+                    @if ($shortcode->description)
+                        <div class=" about-cotent-description body-2 text-variant-1">
+                            {!! BaseHelper::clean(nl2br($shortcode->description)) !!}
+                        </div>
+                    @endif
+                    {{-- <div class="hover-btn-view about-cotent-btn">
+                        @if ($shortcode->button_label)
+                            <a href="{{ $shortcode->button_url }}" class="btn-view style-1">
+                                <span class="text">{{ $shortcode->button_label }}</span>
+                                <span class="icon icon-arrow-right2"></span>
+                            </a>
+                        @endif
+                    </div> --}}
+                    <div class="about-cotent-btn">
+                        @if ($shortcode->button_label)
+                            <a href="{{ $shortcode->button_url }}" class="btn btn-two">
+                           {{ $shortcode->button_label }}       <span class="ms-2 icon icon-arrow-right2"></span>
+                            </a>
+                        @endif
+                    </div>
                 </div>
             </div>
             <div class="col-md-6 my-auto">
-                <div class="banner-video">
+                <div class="about-video banner-video">
                     @if ($shortcode->image)
                         {{ RvMedia::image($shortcode->image, $shortcode->title) }}
                     @endif
